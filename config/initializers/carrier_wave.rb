7b787123595722f9e93f86a6d77cb3ae061d3ec0
Derek Tamsen <dtamsen@gmail.com>
2015-08-30 23:54:22 -0700

    Use ssl for aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,12 +2,13 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon s3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :endpoint              => ENV['S3_ENDPOINT'],
-      :region                => ENV['S3_REGION']
+      :provider                => 'AWS',
+      :aws_access_key_id       => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key   => ENV['S3_SECRET_KEY'],
+      :endpoint                => ENV['S3_ENDPOINT'],
+      :region                  => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory       =  ENV['S3_BUCKET']
+    config.fog_use_ssl_for_aws =  true
   end
 end
